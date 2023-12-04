e0eb766bb88a9a373fd6ff4f1ff32b0f0a47085a
tsshar <61013089+tsshar@users.noreply.github.com>
2023-12-04 09:26:59 +0530

    Rate updated

--- p2.js
+++ p2.js
@@ -3,7 +3,7 @@ function calculateAmount() {
     const books = parseInt(document.getElementById('books').value);
   
     if (!isNaN(pages) && !isNaN(books)) {
-      const amount = books * ((pages * 3) + 350);
+      const amount = books * ((pages * 3) + 300);
       const discount = books*50;
       document.getElementById('result').innerText = `Amount to be paid: ₹${amount} \n Total Discount = ₹${discount}`;
     } else {
